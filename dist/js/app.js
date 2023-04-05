6c06b71b50bb01918265b79a31978b4f62ab6394
Azamat <azamatk9799@gmail.com>
2023-04-05 05:04:28 +0500

    fix-cursor#3

--- dist/js/app.js
+++ dist/js/app.js
@@ -3974,64 +3974,64 @@
     var gsapWithCSS = gsap.registerPlugin(CSSPlugin) || gsap;
     gsapWithCSS.core.Tween;
     document.addEventListener("DOMContentLoaded", (() => {
-        document.body.addEventListener("mousemove", (e => {
-            mouseCoord(e);
-            cursor.classList.remove("hidden");
-            follower.classList.remove("hidden");
-        }));
-        const cursor = document.getElementById("cursor");
-        const follower = document.getElementById("aura");
-        const bullets = document.querySelectorAll(".fp-bullet");
-        let mouseX = 0;
-        let mouseY = 0;
-        let posX = 0;
-        let posY = 0;
         if (isMobile.any()) {
             cursor.hidden = true;
             follower.hidden = true;
         } else {
             cursor.hidden = false;
             follower.hidden = false;
-        }
-        function mouseCoord(e) {
-            mouseX = e.pageX;
-            mouseY = e.pageY;
-        }
-        gsapWithCSS.to({}, {
-            duration: .01,
-            repeat: -1,
-            onRepeat: () => {
-                posX += (mouseX - posX) / 5;
-                posY += (mouseY - posY) / 5;
-                gsapWithCSS.set(cursor, {
-                    css: {
-                        left: mouseX,
-                        top: mouseY
-                    }
-                });
-                gsapWithCSS.set(follower, {
-                    css: {
-                        left: posX - 23,
-                        top: posY - 23
-                    }
-                });
+            const cursor = document.getElementById("cursor");
+            const follower = document.getElementById("aura");
+            const bullets = document.querySelectorAll(".fp-bullet");
+            let mouseX = 0;
+            let mouseY = 0;
+            let posX = 0;
+            let posY = 0;
+            document.body.addEventListener("mousemove", (e => {
+                mouseCoord(e);
+                cursor.classList.remove("hidden");
+                follower.classList.remove("hidden");
+            }));
+            function mouseCoord(e) {
+                mouseX = e.pageX;
+                mouseY = e.pageY;
             }
-        });
-        for (let index = 0; index < bullets.length; index++) {
-            const bullet = bullets[index];
-            bullet.addEventListener("mouseover", (() => {
-                cursor.classList.add("active");
-                follower.classList.add("active");
-            }));
-            bullet.addEventListener("mouseout", (() => {
-                cursor.classList.remove("active");
-                follower.classList.remove("active");
+            gsapWithCSS.to({}, {
+                duration: .01,
+                repeat: -1,
+                onRepeat: () => {
+                    posX += (mouseX - posX) / 5;
+                    posY += (mouseY - posY) / 5;
+                    gsapWithCSS.set(cursor, {
+                        css: {
+                            left: mouseX,
+                            top: mouseY
+                        }
+                    });
+                    gsapWithCSS.set(follower, {
+                        css: {
+                            left: posX - 23,
+                            top: posY - 23
+                        }
+                    });
+                }
+            });
+            for (let index = 0; index < bullets.length; index++) {
+                const bullet = bullets[index];
+                bullet.addEventListener("mouseover", (() => {
+                    cursor.classList.add("active");
+                    follower.classList.add("active");
+                }));
+                bullet.addEventListener("mouseout", (() => {
+                    cursor.classList.remove("active");
+                    follower.classList.remove("active");
+                }));
+            }
+            document.body.addEventListener("mouseout", (() => {
+                cursor.classList.add("hidden");
+                follower.classList.add("hidden");
             }));
         }
-        document.body.addEventListener("mouseout", (() => {
-            cursor.classList.add("hidden");
-            follower.classList.add("hidden");
-        }));
     }));
     window.addEventListener("load", windowLoad);
     function windowLoad() {
--- src/js/libs/gsap.js
+++ src/js/libs/gsap.js
@@ -3,78 +3,73 @@ import { isMobile } from "../files/functions.js";
 
 
 document.addEventListener('DOMContentLoaded', () => {
-
-
-	document.body.addEventListener('mousemove', (e) => {
-		mouseCoord(e);
-		cursor.classList.remove('hidden')
-		follower.classList.remove('hidden')
-	})
-
-	const cursor = document.getElementById('cursor');
-	const follower = document.getElementById('aura');
-	const bullets = document.querySelectorAll('.fp-bullet');
-	let mouseX = 0;
-	let mouseY = 0;
-	let posX = 0;
-	let posY = 0;
-
 	if (isMobile.any()) {
 		cursor.hidden = true;
 		follower.hidden = true;
 	} else {
 		cursor.hidden = false;
 		follower.hidden = false;
+		const cursor = document.getElementById('cursor');
+		const follower = document.getElementById('aura');
+		const bullets = document.querySelectorAll('.fp-bullet');
+		let mouseX = 0;
+		let mouseY = 0;
+		let posX = 0;
+		let posY = 0;
 
-	}
+		document.body.addEventListener('mousemove', (e) => {
+			mouseCoord(e);
+			cursor.classList.remove('hidden')
+			follower.classList.remove('hidden')
+		})
 
-	function mouseCoord(e) {
-		mouseX = e.pageX;
-		mouseY = e.pageY;
-	}
+		function mouseCoord(e) {
+			mouseX = e.pageX;
+			mouseY = e.pageY;
+		}
 
-	gsap.to({}, {
-		duration: 0.01,
-		repeat: -1,
-		onRepeat: () => {
+		gsap.to({}, {
+			duration: 0.01,
+			repeat: -1,
+			onRepeat: () => {
 
-			posX += (mouseX - posX) / 5;
-			posY += (mouseY - posY) / 5;
+				posX += (mouseX - posX) / 5;
+				posY += (mouseY - posY) / 5;
 
-			gsap.set(cursor, {
-				css: {
-					left: mouseX,
-					top: mouseY
-				}
-			});
+				gsap.set(cursor, {
+					css: {
+						left: mouseX,
+						top: mouseY
+					}
+				});
 
-			gsap.set(follower, {
-				css: {
-					left: posX - 23,
-					top: posY - 23
-				}
+				gsap.set(follower, {
+					css: {
+						left: posX - 23,
+						top: posY - 23
+					}
+				})
+			}
+		})
+
+		for (let index = 0; index < bullets.length; index++) {
+			const bullet = bullets[index];
+			bullet.addEventListener('mouseover', () => {
+				cursor.classList.add('active')
+				follower.classList.add('active')
+			})
+
+			bullet.addEventListener('mouseout', () => {
+				cursor.classList.remove('active')
+				follower.classList.remove('active')
 			})
 		}
-	})
 
-	for (let index = 0; index < bullets.length; index++) {
-		const bullet = bullets[index];
-		bullet.addEventListener('mouseover', () => {
-			cursor.classList.add('active')
-			follower.classList.add('active')
-		})
-
-		bullet.addEventListener('mouseout', () => {
-			cursor.classList.remove('active')
-			follower.classList.remove('active')
+		document.body.addEventListener('mouseout', () => {
+			cursor.classList.add('hidden')
+			follower.classList.add('hidden')
 		})
 	}
-
-	document.body.addEventListener('mouseout', () => {
-		cursor.classList.add('hidden')
-		follower.classList.add('hidden')
-	})
-
 })
 
 
